e1209853b05d6a699a9e7b62df9baf56ad305099
facundom <facundomiglierini@gmail.com>
2024-04-21 23:57:34 -0300

    refactored login page

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -4,9 +4,10 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:weather_app/database_controller.dart';
 import 'dart:async';
 import 'package:provider/provider.dart';
-import 'dart:developer' as developer;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+//TODO check for empty credentials on login
 
 void main() async {
   await Hive.initFlutter();
@@ -29,7 +30,7 @@ class WeatherApp extends StatelessWidget {
           colorScheme:
               ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         ),
-        home: HomePage(),
+        home: const HomePage(),
       ),
     );
   }
@@ -104,7 +105,7 @@ class _HomePageState extends State<HomePage> {
       }
     } else {
       selectedIndex = 0;
-      page = WeatherPage();
+      page = const WeatherPage();
     }
     
     return LayoutBuilder(builder: (context, constraints) {
@@ -126,7 +127,7 @@ class _HomePageState extends State<HomePage> {
 
 
 class LoginPage extends StatelessWidget {
-  LoginPage({Key? key, required this.toggleIndex}) : super(key: key);
+  LoginPage({super.key, required this.toggleIndex});
 
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -140,90 +141,166 @@ class LoginPage extends StatelessWidget {
     double deviceHeight(BuildContext context) => MediaQuery.of(context).size.height;
     double deviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
     
-    return Center( 
-      child: Padding(
-        padding: EdgeInsets.only(
-          left: deviceWidth(context) * 0.08,
-          right: deviceWidth(context) * 0.08,
-        ),
-        child: Column( 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TitleCard(),
-            SizedBox(height: 10),
-            const SizedBox(height: 30),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Center( 
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: deviceWidth(context) > 1126 ? deviceWidth(context) * 0.15 : deviceWidth(context) * 0.08,
+              vertical: deviceWidth(context) * 0.08,
             ),
-            const SizedBox(height: 16),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                String message = appState.logIn(_emailController.text, _passwordController.text);
-                ScaffoldMessenger.of(context)
-                  ..hideCurrentSnackBar()
-                  ..showSnackBar(SnackBar(
-                    content: Text(message),
-                    duration: Duration(seconds: 3),
-                  ));
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                minimumSize: Size(
-                  // Set the width of the button to be the same as the text field
-                  MediaQuery.of(context).size.width,
-                  // Set the height of the button
-                  50,
-                ),
-                textStyle: TextStyle(fontSize: 20),
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                foregroundColor: Theme.of(context).colorScheme.onPrimary,
-              ),
-              child: Text('Log in'),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Row(
               children: [
-                SizedBox(width: 10),
-                Text('Don\'t you have an account? ', style: TextStyle(fontSize: 15)),
-                InkWell(
-                  onTap: toggleIndex,
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      decoration: TextDecoration.underline,
-                      fontSize: 15,
+                Visibility(
+                  visible: deviceWidth(context) > 874, 
+                  child: Expanded( 
+                    child: Container(
+                      color: Theme.of(context).colorScheme.background,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 75),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget> [  
+                            Text(  
+                                'Your weather app!',  
+                                style: Theme.of(context).textTheme.displaySmall!.copyWith(color: Theme.of(context).colorScheme.onBackground),  
+                                textAlign: TextAlign.center,
+                            ),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/weather_logo.png',
+                                fit: BoxFit.scaleDown,
+                              ),  
+                            )
+                          ],  
+                        ),
+                      ),
                     ),
                   ),
-                )
-              ],
-            ),
-          ]
+                ),
+                Visibility(
+                  visible: deviceWidth(context) > 874, 
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 45),
+                    child: VerticalDivider(
+                      width: 20,
+                      thickness: 1,
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Expanded( 
+                  child: Container(
+                    color: Theme.of(context).colorScheme.background,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        TitleCard(),
+                        LoginForm(emailController: _emailController, passwordController: _passwordController, appState: appState, toggleIndex: toggleIndex),
+                      ],
+                    ),
+                  ),
+                ),
+              ]
+            )
+          )
+        );
+      }
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    super.key,
+    required TextEditingController emailController,
+    required TextEditingController passwordController,
+    required this.appState,
+    required this.toggleIndex,
+  }) : _emailController = emailController, _passwordController = passwordController;
+
+  final TextEditingController _emailController;
+  final TextEditingController _passwordController;
+  final AppState appState;
+  final VoidCallback toggleIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column( 
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextFormField(
+          controller: _emailController,
+          decoration: const InputDecoration(
+            labelText: 'Email',
+            border: OutlineInputBorder(),
+            contentPadding: EdgeInsets.all(10),
+          ),
         ),
-      )
+        const SizedBox(height: 16),
+        TextFormField(
+          controller: _passwordController,
+          obscureText: true,
+          decoration: const InputDecoration(
+            labelText: 'Password',
+            border: OutlineInputBorder(),
+            contentPadding: EdgeInsets.all(10),
+          ),
+        ),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {
+            String message = appState.logIn(_emailController.text, _passwordController.text);
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(SnackBar(
+                content: Text(message),
+                duration: const Duration(seconds: 3),
+              ));
+          },
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            minimumSize: Size(
+              MediaQuery.of(context).size.width,
+              40,
+            ),
+            textStyle: const TextStyle(fontSize: 15),
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          ),
+          child: const Text('Log in'),
+        ),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(width: 10),
+            const Text('Don\'t you have an account? ', style: TextStyle(fontSize: 15)),
+            InkWell(
+              onTap: toggleIndex,
+              child: Text(
+                'Sign Up',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  decoration: TextDecoration.underline,
+                  fontSize: 15,
+                ),
+              ),
+            )
+          ],
+        ),
+      ]
     );
   }
 }
     
 
 class RegisterPage extends StatelessWidget {
-  RegisterPage({Key? key, required this.toggleIndex}) : super(key: key);
+  RegisterPage({super.key, required this.toggleIndex});
 
   final VoidCallback toggleIndex;
 
@@ -319,7 +396,7 @@ class RegisterPage extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Already have an account? '),
+                      const Text('Already have an account? '),
                       InkWell(
                         onTap: toggleIndex,
                         child: Text(
@@ -391,8 +468,8 @@ class _WeatherPageState extends State<WeatherPage>{
             onPressed: () {
               appState.logOut();              
             }, 
-            icon: Icon(Icons.logout), 
-            label: Text('Log out')),
+            icon: const Icon(Icons.logout), 
+            label: const Text('Log out')),
         ],
       ),
     );
@@ -410,20 +487,27 @@ class TitleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final style = deviceWidth(context) > 380 ? theme.textTheme.displayMedium!.copyWith(
+    final style = deviceWidth(context) > 1000 ? theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onBackground,
+      fontWeight: FontWeight.bold, 
     ) : theme.textTheme.displaySmall!.copyWith(
       color: theme.colorScheme.onBackground,
+      fontWeight: FontWeight.bold, 
     );
 
     return Card(
       color: theme.colorScheme.primaryContainer,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          'Weather App!',
-          style: style,
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Weather App!',
+              style: style,
+            ),
+          ),
+        ],
       ),
     );
   }
